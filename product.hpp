ffdef56b84cb2cd55a1ffd8063d50b4390e349f2
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-05-26 23:24:33 +0300

    Update product.hpp

--- product.hpp
+++ product.hpp
@@ -4,91 +4,60 @@ namespace itertools
 {
     template <typename T1, typename T2>
 
-    /*
-    This class represents a cartesian product of two container-like.
-    */
     class product 
     {
         private:
-        T1 _it1; //Container 1.
-        T2 _it2; //Container 2.
+        T1 cont1;
+        T2 cont2; 
         
         public:
-        /*
-        A copy constructor.
-        */
+  
         product(T1 _start, T2 _end) : _it1(_start), _it2(_end)
         {
-
         }
         
         template <typename P1, typename P2>
-        
-        /*
-        This class represents an iterator.
-        */
         class iterator
         {
             private:
-            P1 data1; //Pointer to the data of the first container.
-            P2 data2; //Pointer to the data of the second container.
-            P2 begin2; //Save the begin of the second iterator.
+            P1 ptr1; 
+            P2 ptr2; 
+            P2 help; 
 
             public:
-            /*
-            A copy constructor.
-            */
-            iterator(P1 ptr1, P2 ptr2) : data1(ptr1), data2(ptr2), begin2(ptr2)
+            iterator(P1 p1, P2 p2) : ptr1(p1), ptr2(p2), help(p2)
             {
-
             }
-
-            /*
-            For operator *:
-            */
-            std::pair<decltype(*data1), decltype(*data2)> operator*() const
+            std::pair<decltype(*ptr1), decltype(*ptr2)> operator*() const
             {
-                return  std::pair<decltype(*data1), decltype(*data2)> (*data1 , *data2);
+                return  std::pair<decltype(*ptr1), decltype(*ptr2)> (*ptr1 , *ptr2);
             }
-
-            /*
-            For operator ++:
-            */
             iterator<P1, P2>& operator++()
             {
-                ++data2; //Advance the second iterator.
+                ++ptr2; 
 			    return *this;
             }
 
-            /*
-            For operator !=:
-            */
 		    bool operator!=(iterator<P1,P2> it)
             {
-                if (!(data2 != it.data2)) //Check if it's the end of the second word.
+                if (!(ptr2 != it.ptr2)) 
                 {
-                    ++data1; //Advance the first iterator.
-                    data2 = begin2; //Place the second iterator at the beginning of the second word.
+                    ++ptr1;
+                    ptr2 = help;
                 }
-			    return ((data1 != it.data1) && (data2 != it.data2)); //Check if it's end of the first word.
+			    return ((ptr1 != it.ptr1) && (ptr2 != it.ptr2)); 
             }
         };
 
         public:
-        /*
-        This function returns the start of the product.
-        */
         auto begin() const
         {
-            return iterator <decltype(_it1.begin()), decltype(_it2.begin())> (_it1.begin(), _it2.begin());
+            return iterator <decltype(cont1.begin()), decltype(cont2.begin())> (cont1.begin(), cont2.begin());
         }
 
-        /*
-        This function returns the end of the product.
-        */
         auto end() const
         {
-            return iterator <decltype(_it1.end()), decltype(_it2.end())> (_it1.end(), _it2.end());
+            return iterator <decltype(cont1.end()), decltype(cont2.end())> (cont1.end(), cont2.end());
         }
     };
 }
