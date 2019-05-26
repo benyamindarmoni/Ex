fe0ec3ba629132611b02818022ca4941d1274241
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-05-26 23:18:38 +0300

    Update zip.hpp

--- zip.hpp
+++ zip.hpp
@@ -5,95 +5,66 @@ namespace itertools
 {
     template <typename T1, typename T2>
 
-    /*
-    This class represents a parallel connection of two container-like (like zip)
-    */
     class zip 
     {
         private:
-        T1 _it1; //Container 1.
-        T2 _it2; //Container 2.
+        T1 cont1;
+        T2 cont2;
         
         public:
-        /*
-        A copy constructor.
-        */
-        zip(T1 _start, T2 _end) : _it1(_start), _it2(_end)
+        
+        zip(T1 start, T2 end1) : cont1(start), cont2(end1)
         {
 
         }
         
         template <typename P1, typename P2>
 
-        /*
-        This class represents an iterator.
-        */
         class iterator
         {
           private:
-            P1 data1; //Pointer to the data of the first container.
-            P2 data2; //Pointer to the data of the second container.
+            P1 ptr1; 
+            P2 ptr2; 
 
             public:
-            /*
-            A copy constructor.
-            */
-            iterator(P1 ptr1, P2 ptr2) : data1(ptr1), data2(ptr2)
+          
+            iterator(P1 p1, P2 p2) : ptr1(p1), ptr2(p2)
             {
-
             }
 
-            /*
-            For operator *:
-            */
-            std::pair<decltype(*data1), decltype(*data2)> operator*() const
+            
+            std::pair<decltype(*ptr1), decltype(*ptr2)> operator*() const
             {
-                return  std::pair<decltype(*data1), decltype(*data2)> (*data1 , *data2);
+                return  std::pair<decltype(*ptr1), decltype(*ptr2)> (*ptr1 , *ptr2);
             }
 
-            /*
-            For operator ++:
-            */
             iterator<P1, P2>& operator++()
             {
-                ++data1; //Advance the first iterator.
-                ++data2; //Advance the second iterator.
+                ++ptr1; 
+                ++ptr2; 
 			    return *this;
             }
 
-            /*
-            For operator !=:
-            */
+          
 		    bool operator!=(iterator<P1,P2> it)
             {
-			    return (data1 != it.data1); //Check if one of the iterators reached the end.
+			    return (ptr1 != it.ptr1);
             }
         };
 
         public:
-
-        /*
-        This function returns the start of the zip.
-        */
         auto begin() const
         {
-            return iterator <decltype(_it1.begin()), decltype(_it2.begin())> (_it1.begin(), _it2.begin());
+            return iterator <decltype(cont1.begin()), decltype(cont2.begin())> (cont1.begin(), cont2.begin());
         }
 
-        /*
-        This function returns the end of the zip.
-        */
         auto end() const
         {
-            return iterator <decltype(_it1.end()), decltype(_it2.end())> (_it1.end(), _it2.end());
+            return iterator <decltype(cont1.end()), decltype(cont2.end())> (cont1.end(), cont2.end());
         }
     };
     
     template <typename T1,typename T2>
-
-    /*
-    For operator <<:
-    */
     ostream &operator<<(ostream &os, const std::pair<T1,T2> &c) 
     {
         os << c.first << "," << c.second;
